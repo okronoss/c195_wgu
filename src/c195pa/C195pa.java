b84642bb79f029a1907c8687d0ef0fb9c53ef247
Alex Desktop <4312702+okronoss@users.noreply.github.com>
2020-04-04 15:38:16 -0700

    Added error message.

--- src/c195pa/C195pa.java
+++ src/c195pa/C195pa.java
@@ -19,7 +19,7 @@ public class C195pa extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/c195pa/views/login.fxml"));
         
         Scene scene = new Scene(root);
         
---
+++ src/c195pa/controllers/LoginController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c195pa.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author alex
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private Button loginBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void exit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void login(ActionEvent event) {
+        // validate username and password
+        
+        // if valid login and change scene to main screen
+        
+        // if not valid show error message.
+    }
+    
+}
